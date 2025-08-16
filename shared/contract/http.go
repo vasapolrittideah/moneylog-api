46859f6b497ab30998f7c07600db6e5e0a7af65e
Vasapol Rittideah <vasapol.rittideah@gmail.com>
2025-08-16 15:23:30 +0700

    feat(contract): add mapping functions for grpc status codes

--- shared/contract/http.go
+++ shared/contract/http.go
@@ -1,6 +1,11 @@
 package contract
 
-import "time"
+import (
+	"net/http"
+	"time"
+
+	"google.golang.org/grpc/codes"
+)
 
 // APIResponse is the response structure for the API.
 type APIResponse struct {
@@ -59,5 +64,71 @@ func NewValidationErrorResponse(details []APIValidationError) APIResponse {
 			Details: details,
 		},
 		Timestamp: time.Now(),
+	}
+}
+
+func ErrorCodeFromGRPCCode(code codes.Code) string {
+	switch code {
+	case codes.OK:
+		return ""
+	case codes.Canceled:
+		return ErrorCodeInternal
+	case codes.Unknown:
+		return ErrorCodeInternal
+	case codes.InvalidArgument:
+		return ErrorCodeBadRequest
+	case codes.DeadlineExceeded:
+		return ErrorCodeInternal
+	case codes.NotFound:
+		return ErrorCodeNotFound
+	case codes.AlreadyExists:
+		return ErrorCodeConflict
+	case codes.PermissionDenied:
+		return ErrorCodeForbidden
+	case codes.ResourceExhausted:
+		return ErrorCodeRateLimit
+	default:
+		return ErrorCodeInternal
+	}
+}
+
+func HTTPStatusFromGRPCCode(code codes.Code) int {
+	switch code {
+	case codes.OK:
+		return http.StatusOK
+	case codes.Canceled:
+		return http.StatusRequestTimeout
+	case codes.Unknown:
+		return http.StatusInternalServerError
+	case codes.InvalidArgument:
+		return http.StatusBadRequest
+	case codes.DeadlineExceeded:
+		return http.StatusGatewayTimeout
+	case codes.NotFound:
+		return http.StatusNotFound
+	case codes.AlreadyExists:
+		return http.StatusConflict
+	case codes.PermissionDenied:
+		return http.StatusForbidden
+	case codes.ResourceExhausted:
+		return http.StatusTooManyRequests
+	case codes.FailedPrecondition:
+		return http.StatusPreconditionFailed
+	case codes.Aborted:
+		return http.StatusConflict
+	case codes.OutOfRange:
+		return http.StatusBadRequest
+	case codes.Unimplemented:
+		return http.StatusNotImplemented
+	case codes.Internal:
+		return http.StatusInternalServerError
+	case codes.Unavailable:
+		return http.StatusServiceUnavailable
+	case codes.DataLoss:
+		return http.StatusInternalServerError
+	case codes.Unauthenticated:
+		return http.StatusUnauthorized
+	default:
+		return http.StatusInternalServerError
 	}
 }
